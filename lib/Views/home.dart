961eacfc70141350ae52ef715cf61f8cf6d985ad
Omar Hatem <omarh.ismail1@gmail.com>
2019-07-18 13:08:43 +0200

    Add expanded Appbar
    * Added Image assets
    * Implemented Expanded Appbar with linear gradient colors

--- lib/Views/home.dart
+++ lib/Views/home.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
+
+  List<Color> colors = [ Colors.pinkAccent, Colors.orange];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        flexibleSpace: Text("HIIIIII"),
+      appBar: PreferredSize(
+        child: Container(
+          height: MediaQuery.of(context).size.height / 1.8,
+          width: MediaQuery.of(context).size.width,
+          child: Image.asset("assets/img/girl1.jpg", height: 50, width: 50,),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(50), bottomRight: Radius.circular(50)),
+              gradient: LinearGradient(
+                colors: colors,
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                stops: [0.1, 1],
+              )
+          ),
+        ),
+        preferredSize: Size(0.0, MediaQuery.of(context).size.height / 1.8),
       ),
     );
   }
