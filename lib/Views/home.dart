f3bf08f91930acb6f1d7fa59d65c6b495216b3f1
Omar Hatem <omarh.ismail1@gmail.com>
2019-07-18 17:37:21 +0200

    Enhance code Readability
    * Removed usless functions
    * divided large widgets into simpler functions

--- lib/Views/home.dart
+++ lib/Views/home.dart
@@ -17,76 +17,41 @@ class Home extends StatelessWidget {
     ),
   ];
 
+  Size size;
+
   @override
   Widget build(BuildContext context) {
+    size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          Container(
-            height: MediaQuery.of(context).size.height / 2,
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(50),
-                    bottomRight: Radius.circular(50)),
-                gradient: LinearGradient(
-                  colors: colors,
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  stops: [0.2, 1],
-                )),
-          ),
-          Positioned(
-            child: ClipRRect(
-              borderRadius: new BorderRadius.circular(30),
-              child: Center(
-                child: Text(
-                  "Find My Soulmate",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 30,
-                      fontFamily: 'Great_Vibes',
-                      fontWeight: FontWeight.bold),
-                ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 410,
+              child: Stack(
+                children: <Widget>[
+                  buildTopGradient(),
+                  buildTitle(),
+                  buildImage(),
+                  AppBar(
+                    backgroundColor: Colors.transparent,
+                    elevation: 0,
+                    actions: <Widget>[
+                      IconButton(
+                        icon: Icon(Icons.notifications),
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ),
-            left: 30,
-            right: 30,
-            top: MediaQuery.of(context).size.height / 6,
-          ),
-          Positioned(
-            child: ClipRRect(
-              borderRadius: new BorderRadius.circular(30),
-              child: Image.asset("assets/img/girl3.jpg"),
+            Container(
+              height: 150,
             ),
-            left: 30,
-            right: 30,
-            top: MediaQuery.of(context).size.height / 3.5,
-          ),
-          AppBar(
-            backgroundColor: Colors.transparent,
-            elevation: 0,
-            actions: <Widget>[
-              IconButton(
-                icon: Icon(Icons.notifications),
-                onPressed: () {},
-              ),
-            ],
-          ),
-          buildBottomRow(context),
-          Positioned(
-            bottom: MediaQuery.of(context).size.height / 12,
-            left: MediaQuery.of(context).size.width / 2.35,
-            child: FloatingActionButton(
-              backgroundColor: Colors.white,
-              onPressed: () {},
-              child: Icon(
-                Icons.favorite,
-                color: Colors.pink,
-              ),
-            ),
-          ),
-        ],
+            buildBottomRow(context),
+          ],
+        ),
       ),
       drawer: buildDrawer(),
     );
@@ -117,59 +82,122 @@ class Home extends StatelessWidget {
     ));
   }
 
-  Widget buildBottomRow(BuildContext context) {
+  Widget buildTopGradient() {
+    return Container(
+      height: 300,
+      width: size.width,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(50),
+              bottomRight: Radius.circular(50)),
+          gradient: LinearGradient(
+            colors: colors,
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            stops: [0.2, 1],
+          )),
+    );
+  }
+
+  Widget buildTitle() {
     return Positioned(
-        bottom: 30,
-        left: 20,
-        right: 20,
-        child: Container(
-          padding: EdgeInsets.only(top: 5, left: 10, right: 10),
-          height: MediaQuery.of(context).size.height / 12,
-          width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(30)),
-              gradient: LinearGradient(
-                colors: colors,
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                stops: [0.2, 1],
-              )),
-          child: Stack(
-            children: <Widget>[
-              Row(
-                children: <Widget>[
-                  IconButton(
-                    icon: Icon(Icons.person),
-                    onPressed: null,
-                    color: Colors.white,
-                    highlightColor: Colors.orange,
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.location_on),
-                    onPressed: null,
-                    color: Colors.white,
-                    highlightColor: Colors.orange,
-                  ),
-                  Spacer(),
-                  IconButton(
-                    icon: Icon(Icons.add),
-                    onPressed: null,
-                    color: Colors.white,
-                    highlightColor: Colors.orange,
-                  ),
-                  IconButton(
-                    icon: Icon(
-                      Icons.message,
-                      color: Colors.white,
-                    ),
-                    onPressed: null,
-                    color: Colors.white,
-                    highlightColor: Colors.orange,
-                  ),
-                ],
-              ),
-            ],
+      child: ClipRRect(
+        borderRadius: new BorderRadius.circular(30),
+        child: Center(
+          child: Text(
+            "Find My Soulmate",
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+                fontFamily: 'Great_Vibes',
+                fontWeight: FontWeight.bold),
           ),
-        ));
+        ),
+      ),
+      left: 30,
+      right: 30,
+      top: 100,
+    );
+  }
+
+  Widget buildImage() {
+    return Positioned(
+      child: ClipRRect(
+        borderRadius: new BorderRadius.circular(30),
+        child: Image.asset(
+          "assets/img/girl3.jpg",
+        ),
+      ),
+      left: 30,
+      right: 30,
+      top: 180,
+    );
+  }
+
+  Widget buildBottomRow(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: 20, right: 20, bottom: 30),
+      height: 90,
+      child: Stack(
+        children: <Widget>[
+          Container(
+            width: size.width,
+            margin: EdgeInsets.only(top: 30),
+            height: 50,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(30)),
+                gradient: LinearGradient(
+                  colors: colors,
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  stops: [0.2, 1],
+                )),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 30),
+            padding: EdgeInsets.only(left: 10, right: 10),
+            child: Row(
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.person, color: Colors.white,),
+                  onPressed: null,
+                ),
+                IconButton(
+                  icon: Icon(Icons.location_on, color: Colors.white,),
+                  onPressed: null,
+                ),
+                Spacer(),
+                IconButton(
+                  icon: Icon(
+                    Icons.add,
+                    color: Colors.white,
+                  ),
+                  onPressed: null,
+                ),
+                IconButton(
+                  icon: Icon(
+                    Icons.message,
+                    color: Colors.white,
+                  ),
+                  onPressed: null,
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 35,
+            left: MediaQuery.of(context).size.width / 2.7,
+            child: FloatingActionButton(
+              backgroundColor: Colors.white,
+              onPressed: () {},
+              child: Icon(
+                Icons.favorite,
+                color: Colors.pink,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
