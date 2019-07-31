78efeed5992c1659a14675fb9c4d82271bde524f
Omar Hatem <omarh.ismail1@gmail.com>
2019-07-31 12:58:14 +0200

    Add minor Changes to Playground UI
    * Added AppBar and ListView.Builder to Stack Widget
    * Changed Items sizes and padding

--- lib/Views/playground.dart
+++ lib/Views/playground.dart
@@ -3,7 +3,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 
 class Playground extends StatelessWidget {
-  var colors = [
+  final colors = [
     Color(0xFFEF7A85),
     Color(0xFFFF90B3),
     Color(0xFFFFC2E2),
@@ -11,7 +11,6 @@ class Playground extends StatelessWidget {
     Color(0xFF826ab0),
     Color(0xFF5927b8),
     Color(0xFFB892FF),
-    Color(0xFFB892FF)
   ];
 
   @override
@@ -20,106 +19,117 @@ class Playground extends StatelessWidget {
     var width = MediaQuery.of(context).size.width;
     int rand;
     return Scaffold(
-      appBar: buildAppBar(width, height),
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, i) {
-          rand = new Random().nextInt(colors.length);
-          return Row(
-            children: <Widget>[
-              Expanded(
-                child: Container(
-                  height: height / 5,
-                  color: colors[rand],
-                ),
-              ),
-              Expanded(
-                flex: 12,
-                child: Column(
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        "Intro of Requiem for a Dream Song.",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                        maxLines: 2,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        "Why must you mortal cut throw the balance that have been so carefully preserved",
-                        style: TextStyle(color: Colors.blueGrey, fontSize: 13),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Expanded(
-                flex: 10,
-                child: Container(
-                  padding: EdgeInsets.only(right: 15),
-                  child: Card(
-                    elevation: 10,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Image.asset('assets/img/girl4.jpg'),
-                    ),
-                  ),
-                  decoration: BoxDecoration(
-                    boxShadow: [
-                      new BoxShadow(
+      body: Container(
+        child: Stack(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(top: height / 3.85),
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: 10,
+                itemBuilder: (context, i) {
+                  rand = new Random().nextInt(colors.length);
+                  return Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Container(
+                          height: height / 5,
                           color: colors[rand],
-                          offset: Offset(20, -20),
-                          spreadRadius: -10)
+                        ),
+                      ),
+                      Expanded(
+                        flex: 12,
+                        child: Column(
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(8, 12, 8, 0),
+                              child: Text(
+                                "Intro of Requiem for a Dream Song.",
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                                maxLines: 2,
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                "Why must you mortal cut throw the balance that have been so carefully preserved.",
+                                style: TextStyle(color: Colors.blueGrey, fontSize: 13),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        flex: 10,
+                        child: Container(
+                          padding: EdgeInsets.only(right: 15),
+                          child: Card(
+                            elevation: 10,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Image.asset('assets/img/girl4.jpg'),
+                            ),
+                          ),
+                          decoration: BoxDecoration(
+                            boxShadow: [
+                              new BoxShadow(
+                                  color: colors[rand],
+                                  offset: Offset(20, -20),
+                                  spreadRadius: -10)
+                            ],
+                          ),
+                        ),
+                      ),
                     ],
-                  ),
-                ),
+                  );
+                },
               ),
-            ],
-          );
-        },
+            ),
+            buildAppBar(width, height),
+          ],
+        ),
       ),
     );
   }
 
   Widget buildAppBar(double width, double height) {
-    return PreferredSize(
-        child: ClipPath(
-            clipper: MyClipper(),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.transparent,
-                image: DecorationImage(
-                    image: AssetImage('assets/img/sand4.jfif'),
-                    fit: BoxFit.fill),
-              ),
-              child: Column(
-                children: <Widget>[
-                  AppBar(
-                    title: Text(
-                      "Playground",
-                      style: TextStyle(color: Colors.blue),
-                    ),
-                    centerTitle: true,
-                    backgroundColor: Colors.transparent,
-                    elevation: 0,
+    return Container(
+      height: height / 3,
+//      color: Colors.blue,
+      child: ClipPath(
+          clipper: MyClipper(),
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/img/sand4.jfif'),
+                  fit: BoxFit.fill),
+            ),
+            child: Column(
+              children: <Widget>[
+                AppBar(
+                  title: Text(
+                    "Playground",
+                    style: TextStyle(color: Colors.blue),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: ListTile(
-                      title: Text("Omar Hatem"),
-                      subtitle: Text("Junior Flutter Developer"),
-                      leading: CircleAvatar(
-                        backgroundImage: AssetImage('assets/img/pp.png'),
-                        radius: 25,
-                      ),
+                  centerTitle: true,
+                  backgroundColor: Colors.transparent,
+                  elevation: 0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    title: Text("Omar Hatem"),
+                    subtitle: Text("Junior Flutter Developer"),
+                    leading: CircleAvatar(
+                      backgroundImage: AssetImage('assets/img/pp.png'),
+                      radius: 25,
                     ),
-                  )
-                ],
-              ),
-            )),
-        preferredSize: Size(width, height / 4));
+                  ),
+                )
+              ],
+            ),
+          )),
+    );
   }
 }
 
